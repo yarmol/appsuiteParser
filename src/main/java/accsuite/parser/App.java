f3f060abd8d967cc62b2ab6cfac90f71afdd9790
yarmol <yarmol@yandex.com>
2015-08-12 12:11:17 +0300

    + constants declarations + road map declaration + parameters declaration

---
+++ src/main/java/accsuite/parser/App.java
@@ -0,0 +1,31 @@
+package accsuite.parser;
+
+/**
+ * 
+ * Class App.
+ * Class to initialize batch of 1c acc.applications 
+ * to upload dt/cf file and dump them into special catalog.
+ * Stages:
+ *  - Parameters to start application:
+ *  	- 1cstart.exe path
+ *  	- git path
+ *  	- working directory (temp bases will be created here)
+ *  	- directory to analize
+ *  - Analizing directory of dt/cf files (using few threads),
+ *  uploading cf or dt file into temporary bases located in working directory
+ *   
+ * @author vitaly
+ * @date 12 рту. 2015 у.
+ */
+
+
+public class App {
+	
+	
+
+	public static void start(String[] args) {
+		///first a
+		
+	}
+
+}
