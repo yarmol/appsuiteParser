fc5f9c28512a938d723432a0fa4063c6922d3313
yarmol <yarmol@yandex.com>
2015-08-12 12:28:01 +0300

    + add docs description

--- src/main/java/accsuite/parser/App.java
+++ src/main/java/accsuite/parser/App.java
@@ -11,8 +11,17 @@ package accsuite.parser;
  *  	- git path
  *  	- working directory (temp bases will be created here)
  *  	- directory to analize
- *  - Analizing directory of dt/cf files (using few threads),
- *  uploading cf or dt file into temporary bases located in working directory
+ *  - Analizing directory (by date) of dt/cf files (using few threads),
+ *    uploading cf or dt file into temporary bases located in working directory.
+ *    
+ *  - Create pool of threads to unload .conf files from every deployed temporary db
+ *  - Create root directory of .conf files with git repo.
+ *  - Day-by-day add .conf files into repo:
+ *      - creating new branch, checkout, add, commit (as old date), merge with master.
+ *  - Analize final .git repo with all dump history of project.
+ *  
+ *  	 
+ *  
  *   
  * @author vitaly
  * @date 12 рту. 2015 у.
