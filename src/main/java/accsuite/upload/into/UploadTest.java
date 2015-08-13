996d00e4faf2b084f659a74c0c0c0abc2fe74031
yarmol <yarmol@yandex.com>
2015-08-13 13:36:01 +0300

    commit

---
+++ src/main/java/accsuite/upload/into/UploadTest.java
@@ -0,0 +1,21 @@
+package accsuite.upload.into;
+
+import junit.framework.TestCase;
+
+public class UploadTest extends TestCase {
+
+	protected static void setUpBeforeClass() throws Exception {
+	}
+
+	protected static void tearDownAfterClass() throws Exception {
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
